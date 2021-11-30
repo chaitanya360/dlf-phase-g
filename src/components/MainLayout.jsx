4ec8a101d24f4eaa5e03d4035bb26eb9ea987bf8
Chaitanya360 <chaitanyajadhav360@gmail.com>
2021-11-30 16:06:33 +0530

    transfered masterplan to top

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -6,39 +6,40 @@ import ModelViewer from "./ModelViewer";
 const data = [
   {
     id: 1,
+    title: "Masterplan",
+    src: "plan",
+  },
+  {
+    id: 2,
     title: "PLAN A",
     src: "https://www.vectary.com/viewer/v1/?model=9ca65df0-a875-4bb3-be39-691c38653f1d&env=studio3",
   },
   {
-    id: 2,
+    id: 3,
     title: "PLAN B",
     src: "https://www.vectary.com/viewer/v1/?model=a0c673fa-c734-4b3a-a15e-e6dc9aae2143&env=studio3",
   },
   {
-    id: 3,
+    id: 4,
     title: "PLAN C",
     src: "https://app.vectary.com/viewer/v1/?model=f43760d2-ac31-48d3-b73f-1fc7df1a26bd&env=studio3",
   },
   {
-    id: 4,
+    id: 5,
     title: "PLAN D",
     src: "https://app.vectary.com/viewer/v1/?model=6d3c433e-a41d-4832-9f36-aafdbd4057cb&env=studio3",
   },
   {
-    id: 5,
+    id: 6,
     title: "PLAN E",
     src: "https://www.vectary.com/viewer/v1/?model=e4b996d1-acdd-4d8a-8ec8-04ee0fdb0deb&env=studio3",
   },
   {
-    id: 6,
+    id: 7,
     title: "PLAN F",
     src: "https://www.vectary.com/viewer/v1/?model=e0b61a5e-0d87-403a-a8fa-3f9bcd857885&env=studio3",
   },
-  {
-    id: 7,
-    title: "Masterplan",
-    src: "plan",
-  },
+
   {
     id: 8,
     title: "Virtual Tour",
