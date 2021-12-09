ccac84c8180f73b583f1ff9d5335cd2d500f673f
Chaitanya360 <chaitanyajadhav360@gmail.com>
2021-12-09 19:28:44 +0530

    now font family will change based on class name

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import { HeaderStyle } from "./components.style";
 
 function Header(props) {
-  return <HeaderStyle>Signature Residences</HeaderStyle>;
+  return (
+    <HeaderStyle className="vivaldi-font">Signature Residences</HeaderStyle>
+  );
 }
 
 export default Header;
