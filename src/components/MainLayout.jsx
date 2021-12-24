5306e9e8b7034d4735bdf52db93f3b1073b6a01d
Chaitanya360 <chaitanyajadhav360@gmail.com>
2021-12-24 17:12:46 +0530

    changed virtual tour link

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -49,7 +49,8 @@ const data = [
   {
     id: 8,
     title: "Virtual Tour",
-    src: "https://btvrprojects.s3.ap-south-1.amazonaws.com/Dlf_3rd-F+%26+Un-f/index.htm",
+    src: "https://btvrprojects.s3.ap-south-1.amazonaws.com/Vatika_seven+lamps-tour/index.htm",
+
     // isTour is just for commit purpose will be deleted
     isTour: true,
   },
